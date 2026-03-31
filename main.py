ab57b94058313a3670223d706c26895253a21b4b
HCMacaneiro <hcmacaneiro@gmail.com>
2026-03-30 23:09:44 -0300

    Add argument logic to script

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import sys
+
+
 def main():
     print("Hello from py-fim!")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <dir_to_monitor>")
+        sys.exit(1)
     main()
