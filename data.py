f414dd639e082516b85d43a7c3f9dba7eb455f36
HCMacaneiro <hcmacaneiro@gmail.com>
2026-04-05 12:15:24 -0300

    Create Diff class and create diff report

--- data.py
+++ data.py
@@ -15,3 +15,41 @@ class Baseline():
             print(e)
             return False
         return True 
+
+
+class Diff():
+    def __init__(self, base_dir, new_data):
+        self.base_dir = Path(base_dir)
+        str_base_dir = str(self.base_dir.resolve())
+        self.new_data = new_data.get(str_base_dir, {})
+
+    def get_old_data(self):
+        baseline_path = Path(__file__).parent / "baseline.json"
+        with baseline_path.open("r") as f:
+            current_baseline = json.load(f)
+        target_dir = str(self.base_dir.resolve())
+        return current_baseline.get(target_dir, {})
+    
+    def report_diff(self, old_data):
+        old_data_keys = set(old_data.keys())
+        new_data_keys = set(self.new_data.keys())
+        added_files = new_data_keys - old_data_keys
+        removed_files = old_data_keys - new_data_keys
+        unchanged_files = set()
+        changed_files = set()
+        for file in new_data_keys.intersection(old_data_keys):
+            if self.new_data[file] == old_data[file]:
+                unchanged_files.add(file)
+            else:
+                changed_files.add(file)
+        return f"""
+        Added: {len(added_files)}
+        Removed: {len(removed_files)}
+        Changed: {len(changed_files)}
+
+        ---
+        Added files: {", ".join(added_files) or None}
+        Removed files: {", ".join(removed_files) or None}
+        Changed files: {", ".join(changed_files) or None}
+        """
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from engine import Scanner
+from data import Diff
 
 
 def main():
@@ -7,12 +8,13 @@ def main():
     scan = Scanner(directory)
     found_files = scan.get_files()
     if scan.check_baseline():
-        print("Baseline already exists.")
+        diff = Diff(directory, found_files)
+        old_baseline = diff.get_old_data()
+        report = diff.report_diff(old_baseline)
+        print(report)
         sys.exit(1)
     baseline = scan.save_scan(found_files)
     
-
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
